3860672e2daad001603778fd3405326888124ab0
lhje <theeluke@gmail.com>
2023-03-20 16:31:27 -0600

    Add `widgets_ratings_controller.rb`, update `widgets/show.html.erg` ... and `routes.rb`, adding in the ability to create a Widget Rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :widgets, only: [ :show, :index ]
+  resources :widget_ratings, only: [ :create  ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
