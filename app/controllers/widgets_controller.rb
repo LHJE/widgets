1cee4fc5301a721356efce67cfc511541fe8c03a
lhje <theeluke@gmail.com>
2023-03-20 14:34:29 -0600

    Add `widgets_controller` and corresponding `show.html.erb`

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,8 @@
+class WidgetsController < ApplicationController
+  def show
+    @widget = OpenStruct.new(id: params[:id],
+                             manufacturer_id: rand(100),
+                             name: "Widget #{params[:id]}")
+
+  end
+end
