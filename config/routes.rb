dcb67e88b54b346a54cbab41e01a84ee4396aeaa
lhje <theeluke@gmail.com>
2023-03-20 14:34:05 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :widgets
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "manufacturer/:id", to: "manufacturers#show"
 end
