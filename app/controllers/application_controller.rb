f85c051fdc69453caa5d02eb24f48a890da5d622
lhje <theeluke@gmail.com>
2023-08-01 16:17:11 -0600

    Update `application_controller.rb` with `current_user` method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    User.find_by(id: session[:current_user_id])
+  end
+  helper_method :current_user
+  
 end
